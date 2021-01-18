023df5af503d6577be1f9dc853db60453dc1867b
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-01-19 00:09:35 +0200

    feat: sidebar elemnets

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -4,10 +4,24 @@ import styles from './sidebar.module.css'
  * @param {Object} props props for sidebar components
  * @param {React.Component} props.children 
  */
-export default function Sidebar(props) {
+export function Sidebar(props) {
     return (
         <nav className={`${styles["sidebar"]} bg-dark text-light`}>
             {props.children}
         </nav>
     );
+}
+
+
+/**
+ * 
+ * @param {Object} props
+ * @param {React.Component} icon
+ * @param {string} text
+ */
+export function SidebarElement({icon, text}) {
+   return ( <div style={{display: "flex", flexDirection:"column", alignItems:"center"}}>
+       {icon}
+       {text}
+   </div>  );
 }
--- pages/professor/dashboard.js
+++ pages/professor/dashboard.js
@@ -1,16 +1,18 @@
-import Sidebar from "../../components/sidebar/sidebar"
+import { Sidebar, SidebarElement } from "../../components/sidebar/sidebar"
 import styles from "./dashboard.module.css"
 export default function Dashboard() {
     return (
         <>
-<div className={`${styles["dashboard-container"]}`}>
-            <Sidebar>
-                djskaldj
+            <div className={`${styles["dashboard-container"]}`}>
+                <Sidebar>
+                    <SidebarElement icon={
+                        <i class="bi bi-house"></i>
+                    } text={"Home"}></SidebarElement>
                 </Sidebar>
-            <main className={`${styles["content"]}`}>
-                Main stuff
+                <main className={`${styles["content"]}`}>
+                    Main stuff
                     </main>
-</div>
+            </div>
         </>
     )
 }
