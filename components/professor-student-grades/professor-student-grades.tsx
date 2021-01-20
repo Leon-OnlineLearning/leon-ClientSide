d384f392c320809bb893663e467390f92d1181f0
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-01-20 13:58:41 +0200

    feat: add grades

--- components/professor-student-grades/professor-student-grades.tsx
+++ components/professor-student-grades/professor-student-grades.tsx
@@ -1,22 +1,18 @@
-import styles from "./student-attendance.module.css"
+import styles from "./student-grades.module.css"
 import Dropdown from "react-bootstrap/Dropdown"
 import ButtonGroup from "react-bootstrap/ButtonGroup"
 import Button from "react-bootstrap/Button"
 import { useState } from "react";
 import { Table } from "react-bootstrap";
-export default function ProfessorStudentAttendance() {
+export default function ProfessorStudentGrades() {
     const [yearName, setYearName] = useState("Years");
     const [courseName, setCourseName] = useState("Course Name");
-    const [lectureName, setLectureName] = useState("Lecture");
 
     const onYearsSelected = (eventKey: string) => {
         setYearName(eventKey);
     }
     const onCourseSelected = (eventKey: string) => {
         setCourseName(eventKey);
-    }
-    const onLectureSelected = (eventKey: string) => {
-        setLectureName(eventKey);
     }
     return (
         <>
@@ -39,34 +35,34 @@ export default function ProfessorStudentAttendance() {
                         <Dropdown.Item eventKey="Course 2" >Course 2</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
-                <Dropdown onSelect={onLectureSelected} className={`${styles["dropdown-area"]}`} >
-                    <Dropdown.Toggle variant="primary" id="courses" className={`${styles["dropdown-btn"]}`}>
-                        {lectureName}
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu style={{ width: "100%" }}>
-                        <Dropdown.Item eventKey="Lecture 1" >Lecture 1</Dropdown.Item>
-                        <Dropdown.Item eventKey="Lecture 2" >Lecture 2</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
             </div>
             <Table hover bordered className={`${styles["attendance-table"]}`}>
                 <thead>
                     <tr>
                         <th>Name</th>
-                        <th>Attendance</th>
+                        <th>Exam 1</th>
+                        <th>Exam 2</th>
+                        <th>Sum</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <th>Ahmed</th>
-                        <th>80%</th>
+                        <th>10</th>
+                        <th>10</th>
+                        <th>20</th>
                     </tr>
                     <tr>
                         <th>Ahmed2</th>
-                        <th>90%</th>
+                        <th>10</th>
+                        <th>15</th>
+                        <th>25</th>
                     </tr>
                 </tbody>
             </Table>
+            <div className="p-2" style={{ display: "flex", justifyContent: "center" }}>
+                Overall performance: <b>45/50 = 90%</b>
+            </div>
             <div style={{ display: "flex", justifyContent: "center" }}>
                 <ButtonGroup aria-label="Pages" className="mx-2">
                     <Button>1</Button> <Button variant="secondary">2</Button> <Button variant="secondary">3</Button> <Button variant="secondary">4</Button>
--- pages/professor/grades.js
+++ pages/professor/grades.js
@@ -1,9 +1,12 @@
 import { ProfessorDashboard, ProfessorDashboardSelectedPage } from "../../components/dashboard-professor/professor-dashboard";
+import ProfessorStudentGrades from "../../components/professor-student-grades/professor-student-grades"
 
 export default function Dashboard() {
     return (
         <>
-            <ProfessorDashboard selectedPage={ProfessorDashboardSelectedPage.grads}></ProfessorDashboard>
+            <ProfessorDashboard selectedPage={ProfessorDashboardSelectedPage.grads}>
+                <ProfessorStudentGrades></ProfessorStudentGrades>
+            </ProfessorDashboard>
         </>
     )
 }
