e41531521054ee7cdc002c631687434790366c1d
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-01-25 18:44:27 +0200

    feat: create student view

--- components/student/student-attendace/student-attendance.tsx
+++ components/student/student-attendace/student-attendance.tsx
@@ -1,33 +1,20 @@
-import styles from "./student-grades.module.css"
+import styles from "./student-attendance.module.css"
 import Dropdown from "react-bootstrap/Dropdown"
 import ButtonGroup from "react-bootstrap/ButtonGroup"
 import Button from "react-bootstrap/Button"
 import { useState } from "react";
 import { Table } from "react-bootstrap";
-export default function ProfessorStudentGrades() {
-    const [yearName, setYearName] = useState("Years");
+export default function StudentAttendance() {
     const [courseName, setCourseName] = useState("Course Name");
 
-    const onYearsSelected = (eventKey: string) => {
-        setYearName(eventKey);
-    }
     const onCourseSelected = (eventKey: string) => {
         setCourseName(eventKey);
     }
     return (
         <>
             <div className="m-3">
-                <h2 className="m-2">Students Grades</h2>
+                <h2 className="m-2">Your Attendance</h2>
                 <span className={`${styles["dropdown-container"]}`}>
-                    <Dropdown onSelect={onYearsSelected} className={`${styles["dropdown-area"]}`}>
-                        <Dropdown.Toggle variant="primary" id="years" className={`${styles["dropdown-btn"]}`}>
-                            {yearName}
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu className={`${styles["dropdown-item"]}`}>
-                            <Dropdown.Item eventKey="Year1" >Year1</Dropdown.Item>
-                            <Dropdown.Item eventKey="Year2" >Year2</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
                     <Dropdown onSelect={onCourseSelected} className={`${styles["dropdown-area"]}`}>
                         <Dropdown.Toggle variant="primary" id="courses" className={`${styles["dropdown-btn"]}`}>
                             {courseName}
@@ -41,30 +28,33 @@ export default function ProfessorStudentGrades() {
                 <Table hover bordered className={`${styles["attendance-table"]}`}>
                     <thead>
                         <tr>
-                            <th>Name</th>
-                            <th>Exam 1</th>
-                            <th>Exam 2</th>
-                            <th>Sum</th>
+                            <th>Lecture Title </th>
+                            <th>Attended</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <th>Ahmed</th>
-                            <th>10</th>
-                            <th>10</th>
-                            <th>20</th>
+                            <th>Lecture 1</th>
+                            <th><i className="bi bi-check-circle-fill"></i></th>
                         </tr>
                         <tr>
-                            <th>Ahmed2</th>
-                            <th>10</th>
-                            <th>15</th>
-                            <th>25</th>
+                            <th>Lecture 2</th>
+                            <th>{" "}</th>
+                        </tr>
+                        <tr>
+                            <th>Lecture 3</th>
+                            <th><i className="bi bi-check-circle-fill"></i></th>
+                        </tr>
+                        <tr>
+                            <th>Lecture 4</th>
+                            <th><i className="bi bi-check-circle-fill"></i></th>
+                        </tr>
+                        <tr>
+                            <th>Lecture 5</th>
+                            <th>{" "}</th>
                         </tr>
                     </tbody>
                 </Table>
-                <span className="p-2" style={{ display: "flex", justifyContent: "center" }}>
-                    Overall performance: <b>45/50 = 90%</b>
-                </span>
                 <span style={{ display: "flex", justifyContent: "center" }}>
                     <ButtonGroup aria-label="Pages" className="mx-2">
                         <Button>1</Button> <Button variant="secondary">2</Button> <Button variant="secondary">3</Button> <Button variant="secondary">4</Button>
