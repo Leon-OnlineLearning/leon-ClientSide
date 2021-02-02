0a03ea8b47b5f03d34e0994507560ab4f6105c15
abdo ragab hashem <ar1813@fayoum.edu.eg>
2021-02-03 00:12:39 +0200

    add comment for pointer canvas

--- components/stream/viewer/pointer_canvas.tsx
+++ components/stream/viewer/pointer_canvas.tsx
@@ -1,7 +1,19 @@
 import { useEffect, useRef } from "react";
 
 import pointer_img from "./pointer.svg"
-
+/**
+ * this will create a canvas in absolute position 
+ * which can be used to draw other other elements
+ * would require a parent div with position `relative` draw in sibling
+ * 
+ * 
+ * @param height the height of the canvas to draw
+ * @param width the width of the canvas to draw
+ * @param pointerX x position of the pointer
+ * @param pointerY y position of the pointer
+ *  
+ * @returns
+ */
 export default function Pointer_canvas({
   height,
   width,
