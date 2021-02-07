ef616b5402bc35a2ee304c1ca0e66b2194e1bba3
Abdullah <Khaled>
2021-02-07 17:05:53 +0200

    refact: open close princible for new accounts

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,18 +1,27 @@
-import { AdminDashboard, AdminDashboardSelectedPage  } from "../../components/admin/dashboard/admin-dashboard"
+import {
+  AdminDashboard,
+  AdminDashboardSelectedPage,
+} from "../../components/admin/dashboard/admin-dashboard";
 import ButtonsListLayout from "../../components/buttons-list-layout/buttons-list-layout";
-import AccountSettings from "../../components/accounts-settings/account-settings"
+import AccountSettings from "../../components/accounts-settings/account-settings";
+import CreateNewAccount from "../../components/admin/new-account/new-account";
+import newStudentComponentGenerator from "../../components/admin/new-account/new-student";
 
 export default function Dashboard() {
-    return (
-        <AdminDashboard selectedPage={AdminDashboardSelectedPage.accounts}>
-            <ButtonsListLayout pages={
-                {
-                    "Create new accounts": <AccountSettings />,
-                    "Manage privileges": <AccountSettings />,
-                    "Update account settings": <AccountSettings />,
-                    "Manage Users": <AccountSettings />,
-                }
-            }></ButtonsListLayout>
-        </AdminDashboard>
-    )
+  return (
+    <AdminDashboard selectedPage={AdminDashboardSelectedPage.accounts}>
+      <ButtonsListLayout
+        pages={{
+          "Create new accounts": (
+            <CreateNewAccount
+              privilagesComponents={{ student: newStudentComponentGenerator }}
+            />
+          ),
+          "Manage privileges": <AccountSettings />,
+          "Update account settings": <AccountSettings />,
+          "Manage Users": <AccountSettings />,
+        }}
+      ></ButtonsListLayout>
+    </AdminDashboard>
+  );
 }
