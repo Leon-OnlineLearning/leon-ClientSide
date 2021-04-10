2e0cbe8a24d7500d80c2b1744b051fc2b8953520
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-04-10 18:46:44 +0200

    feat: make user more flexible

--- model/users/User.ts
+++ model/users/User.ts
@@ -1,40 +1,10 @@
-export default abstract class User {
+export default class User {
   constructor(
-    private _firstName: string,
-    private _lastName: string,
-    private _email: string,
-    private _role: string,
-    private _id?: string,
+    public firstName: string,
+    public lastName: string,
+    public email: string,
+    public password: string,
+    public id?: string,
   ) {}
-
-  public get firstName(): string {
-    return this._firstName;
-  }
-
-  public set firstName(v: string) {
-    this._firstName = v;
-  }
-
-  public get lastName() {
-    return this._lastName;
-  }
-
-  public set lastName(v: string) {
-    this._lastName = v;
-  }
-
-  public get email() : string {
-    return this._email
-  }
-
-  public get id(): string {
-    return this._id;
-  }
-
-  
-  public get role() : string {
-    return this._role
-  }
-  
 }
 
