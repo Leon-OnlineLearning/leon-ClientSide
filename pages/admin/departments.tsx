a7cd9601fddf4d063a7607527b2ce0f4bbe1334d
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-04-11 12:03:30 +0200

    feat: edit department

--- pages/admin/departments.tsx
+++ pages/admin/departments.tsx
@@ -1,8 +1,26 @@
-import { AdminDashboard, AdminDashboardSelectedPage } from "../../components/admin/dashboard/admin-dashboard";
+import {
+  AdminDashboard,
+  AdminDashboardSelectedPage,
+} from "../../components/admin/dashboard/admin-dashboard";
+import ListLayout from "../../components/list-layout/list-layout";
+import {
+  editDepartment,
+  deleteDeprtment,
+  getDepartments,
+  addNewDepartment
+} from "../../controller/departments";
 
 export default function Dashboard() {
     return (
-        <AdminDashboard selectedPage={AdminDashboardSelectedPage.departments}>
-        </AdminDashboard>
-    )
+      <AdminDashboard selectedPage={AdminDashboardSelectedPage.departments}>
+        <ListLayout
+          title="Departments"
+          onFetchItems={getDepartments}
+          onEditItem={editDepartment}
+          onAddNewItem={addNewDepartment}
+          onDeleteItem={deleteDeprtment}
+        />
+      </AdminDashboard>
+    );
 }
+
