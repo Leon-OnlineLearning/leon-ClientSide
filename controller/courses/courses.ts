58d92d0ce7a7a1f14a6d2f9bc9e676719fee8a7f
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-04-11 13:23:53 +0200

    feat: add course

--- controller/courses/courses.ts
+++ controller/courses/courses.ts
@@ -1,7 +1,36 @@
-export async function getAllCourses(){
-	return await [
-		"course1",
-		"course2",
-		"course3",
-	]
+import axios from "axios";
+import Item from "../../model/Item";
+import config from "../../utils/config";
+
+export async function getAllCourses() {
+  return await axios
+    .get(`${config.serverBaseUrl}/courses`, { withCredentials: true })
+    .then((response) => response.data);
 }
+
+export async function editCourse(oldCourse: Item, newCourse: Item) {
+  await axios.put(
+    `${config.serverBaseUrl}/courses/${oldCourse.id}`,
+    newCourse,
+    { withCredentials: true }
+  );
+}
+
+export async function addNewCourse(title: string) {
+  return await axios
+    .post(
+      `${config.serverBaseUrl}/courses/`,
+      { name: title },
+      {
+        withCredentials: true,
+      }
+    )
+    .then((response) => response.data);
+}
+
+export async function deleteCourse(course: Item) {
+  await axios.delete(`${config.serverBaseUrl}/courses/${course.id}`, {
+    withCredentials: true,
+  });
+}
+
--- pages/admin/courses.tsx
+++ pages/admin/courses.tsx
@@ -1,8 +1,21 @@
-import { AdminDashboard, AdminDashboardSelectedPage } from "../../components/admin/dashboard/admin-dashboard";
+import {
+  AdminDashboard,
+  AdminDashboardSelectedPage,
+} from "../../components/admin/dashboard/admin-dashboard";
+import ListLayout from "../../components/list-layout/list-layout";
+import {getAllCourses, editCourse, addNewCourse, deleteCourse} from "../../controller/courses/courses";
 
 export default function Dashboard() {
     return (
-        <AdminDashboard selectedPage={AdminDashboardSelectedPage.courses}>
-        </AdminDashboard>
-    )
+      <AdminDashboard selectedPage={AdminDashboardSelectedPage.courses}>
+        <ListLayout
+          title="Courses"
+          onFetchItems={getAllCourses}
+          onEditItem={editCourse}
+          onAddNewItem={addNewCourse}
+          onDeleteItem={deleteCourse}
+        />
+      </AdminDashboard>
+    );
 }
+
