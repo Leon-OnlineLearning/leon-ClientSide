ac47e165af3006b898034bb00d1d8f93f7b5bf77
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-04-12 12:29:12 +0200

    feat: get events

--- controller/getEvent.ts
+++ controller/getEvent.ts
@@ -1,13 +1,17 @@
 import axios from "axios"
-import { Event, EventType } from "../model/event"
+import { Event } from "../model/event"
 import config from "../utils/config"
 
-export function getEvents(year: number, month: number): Promise<Event[]> {
+export async function getEvents(year: number, month: number): Promise<Event[]> {
     const startingFrom = `${year}-${month}-01`
     const endingAt = `${year}-${month + 1}-01`
-    return axios.get(`${config.serverBaseUrl}/students/${localStorage.getItem('id')}/events?startingFrom=${startingFrom}&endingAt=${endingAt}`)
-        .then(response => {
-            console.log(response.data);
-            return response.data
-        })
+    const response = await axios
+    .get(
+      `${config.serverBaseUrl}/students/${localStorage.getItem(
+        "id"
+      )}/events?startingFrom=${startingFrom}&endingAt=${endingAt}`,
+      {withCredentials: true}
+    )
+  console.log(response.data)
+  return response.data
 }
