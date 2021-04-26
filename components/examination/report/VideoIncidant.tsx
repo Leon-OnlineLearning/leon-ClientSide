f977cb6f5d088934c8ba55d2d549bf5c9787bf37
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2021-04-26 02:55:32 +0200

    typo

--- components/examination/report/VideoIncidant.tsx
+++ components/examination/report/VideoIncidant.tsx
@@ -6,7 +6,7 @@ export default function VideoIncidant(props:{
 }){
     return  (<>
     <h1>start time : {props.incident.startTime}</h1>
-    <h1>start time : {props.incident.endTime}</h1>
+    <h1>end time : {props.incident.endTime}</h1>
     <br></br>
     </>);
 }
