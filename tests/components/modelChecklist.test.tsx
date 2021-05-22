f6f1df2c6beb58e1c2b6de556f9dc7441e886d4d
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-05-22 12:20:44 +0200

    feat: mode classes checklist

--- tests/components/modelChecklist.test.tsx
+++ tests/components/modelChecklist.test.tsx
@@ -1,9 +1,19 @@
+import { render, waitFor } from "@testing-library/react";
+import axios from "axios";
 import { rest } from "msw";
-import { setupServer } from "msw/lib/types/node";
+import { setupServer } from "msw/node";
+import ModelChecklist from "../../components/models-checklist/modelsCheckList";
+import { screen } from "@testing-library/react";
+import ModelClasses from "../../model/ModelClasses";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
 
 describe("Model check list test suite", () => {
+  let modelIdCorrect = false;
+  let classesAreCorrect = true;
   const server = setupServer(
     rest.get("/models/:modelId/classes", (req, res, ctx) => {
+      modelIdCorrect = req.params.modelId === "ModelId";
       return res(
         ctx.json([
           {
@@ -14,16 +24,17 @@ describe("Model check list test suite", () => {
             id: "12346",
             name: "class 2",
           },
-          {
-            id: "12347",
-            name: "class 3",
-          },
-          {
-            id: "12348",
-            name: "class 4",
-          },
         ])
       );
+    }),
+    rest.put("/models/:modelId/classes", (req, res, ctx) => {
+      const classes: any = req.body;
+      classes.forEach((_class) => {
+        classesAreCorrect =
+          classesAreCorrect &&
+          (_class.name === "class 1" || _class.name === "class 2");
+      });
+      return res(ctx.json({ success: false }));
     })
   );
   beforeAll(() => {
@@ -34,5 +45,55 @@ describe("Model check list test suite", () => {
   });
   afterAll(() => {
     server.close();
+  });
+
+  const contentFetcher = async (modelId) => {
+    return await axios
+      .get(`/models/${modelId}/classes`)
+      .then((resp) => resp.data as ModelClasses[]);
+  };
+
+  const retrainHandler = async (modelId: string, classes: ModelClasses[]) => {
+    return await axios
+      .put(`/models/${modelId}/classes`, classes)
+      .then((resp) => resp.data);
+  };
+
+  const prepareView = async () => {
+    await waitFor(() => screen.getByTestId("classes"));
+  };
+
+  const doneSendingClasses = async () => {
+    await waitFor(() => screen.getByText("done"));
+  };
+
+  test("should get content from the server and display it", async () => {
+    render(
+      <ModelChecklist
+        onRetrain={retrainHandler}
+        contentFetcher={contentFetcher}
+        modelId="ModelId"
+      />
+    );
+    await prepareView();
+    expect(screen.getByText("class 1")).toBeVisible();
+    expect(screen.getByText("class 2")).toBeVisible();
+    expect(modelIdCorrect).toBeTruthy();
+  });
+
+  test("should send data correctly to the backend server", async () => {
+    render(
+      <ModelChecklist
+        onRetrain={retrainHandler}
+        contentFetcher={contentFetcher}
+        modelId="ModelId"
+      />
+    );
+    await prepareView();
+    userEvent.click(screen.getByText("class 1"));
+    userEvent.click(screen.getByText("class 2"));
+    userEvent.click(screen.getByText("Retrain"));
+    await doneSendingClasses();
+    expect(classesAreCorrect).toBeTruthy();
   });
 });
