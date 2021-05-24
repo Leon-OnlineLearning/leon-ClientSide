cb16a22791133a911d3e7e4e8f82b4ae2e96eaeb
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2021-05-24 16:50:41 +0200

    create page for exam creation

--- pages/professor/exams.tsx
+++ pages/professor/exams.tsx
@@ -1,10 +1,17 @@
 import ProfessorStudentAttendance from "../../components/professor/student-attendace/professor-student-attendance"
 import {ProfessorDashboard, ProfessorDashboardSelectedPage} from "../../components/professor/dashboard/professor-dashboard";
+import React from "react";
+import { Button } from "react-bootstrap";
+import router from "next/router";
 
 export default function Dashboard() {
+    
+    // TODO view created exams
     return (
         <>
-            <ProfessorDashboard selectedPage={ProfessorDashboardSelectedPage.exams}></ProfessorDashboard>
+            <ProfessorDashboard selectedPage={ProfessorDashboardSelectedPage.exams}>
+                <Button onClick={()=>{router.push('/professor/createExam')}}>add exam</Button>
+            </ProfessorDashboard>
         </>
     )
 }
