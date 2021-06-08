9b078bc2a2375204c013660cef019ebc97505635
abdo hashem <ar1813@fayoum.edu.eg>
2021-06-08 15:58:21 +0200

    feat: support video at report

--- components/examination/report/VideoIncidant.tsx
+++ components/examination/report/VideoIncidant.tsx
@@ -1,12 +1,27 @@
+import config from '../../../utils/config'
+
+
+
+
+
 export default function VideoIncidant(props:{
     incident: {
         startingFrom: number,
         endingAt: number
-    }
+    },
+    examId: string
+    userId: string
 }){
+
+    const duration = props.incident.endingAt-props.incident.startingFrom
+
+    const query_params = `startingTime=${props.incident.startingFrom}&duration=${duration}&userId=${props.userId}&examId=${props.examId}`
+    const url = `${config.serverBaseUrl}/exams/video?${query_params}`
+    console.debug(url)
     return  (<>
     <h1>start time : {props.incident.startingFrom}</h1>
     <h1>end time : {props.incident.endingAt}</h1>
     <br></br>
+    <video id="videoPlayer" controls src={url} preload="metadata"/>
     </>);
 }
