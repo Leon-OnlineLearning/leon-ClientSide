a6b677f7e8aac4089e6ac35f1224d6034ba942f1
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-07-04 13:51:27 +0200

    fix: get all courses instead of lectures

--- controller/user/professor/professor.ts
+++ controller/user/professor/professor.ts
@@ -34,7 +34,7 @@ export async function assignLectureToProfessor(
 
 export async function getAllCoursesByProfessor(professorId: string) {
   return await apiInstance
-    .get(`/professors/${professorId}/lectures`, {
+    .get(`/professors/${professorId}/courses`, {
       withCredentials: true,
     })
     .then((resp) => resp.data);
