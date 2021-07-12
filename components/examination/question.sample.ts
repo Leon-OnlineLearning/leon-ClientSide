d3d71fa68bc6eb4e1b6fc41408d1940be4893ec7
abdo hashem <ar1813@fayoum.edu.eg>
2021-07-13 00:42:45 +0200

    fix: wrong type

--- components/examination/question.sample.ts
+++ components/examination/question.sample.ts
@@ -1,7 +1,6 @@
 import { QuestionInterface, Q_type } from "../../model/examination/question"
 
 export let example_multi_question :QuestionInterface= {
-    questionId : 123,
     questionText : "question text need to choose many",
     questionType : Q_type.MultiChoice,
     choices : [
@@ -16,28 +15,28 @@ export let example_multi_question :QuestionInterface= {
 
 
 export let example_short_answer :QuestionInterface= {
-    questionId : 124,
+    
     questionText : "question text needs short answer",
     questionType : Q_type.ShortAnswer
 }
 
 
 export let example_LongAnswer :QuestionInterface= {
-    questionId : 1241,
+    
     questionText : "question text needs long answer",
     questionType : Q_type.LongAnswer
 }
 
 
 export let example_code :QuestionInterface= {
-    questionId : 1234,
+    
     questionText : "question text needs code as answer",
     questionType : Q_type.Code
 }
 
 
 export let example_single_choice :QuestionInterface= {
-    questionId : 1234,
+    
     questionText : "question text needs single choice ",
     questionType : Q_type.SingleChoice,
     choices : [
@@ -50,7 +49,7 @@ export let example_single_choice :QuestionInterface= {
 
 
 export let example_image :QuestionInterface= {
-    questionId : 1234,
+    
     questionText : "question text needs single choice ",
     questionType : Q_type.Image
 }
