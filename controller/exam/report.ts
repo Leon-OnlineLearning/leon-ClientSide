fe88fda159b42281393fbe66e341b16a332e2899
abdo hashem <ar1813@fayoum.edu.eg>
2021-07-13 00:43:21 +0200

    add report type for incident

--- controller/exam/report.ts
+++ controller/exam/report.ts
@@ -1,7 +1,8 @@
+import { IncidentInterface } from "../../model/examination/report";
 import apiInstance from "../utils/api";
 
 export async function getExamReport(userId, examId){
-    const res = await apiInstance.get(`/exams/report`,
+    const res = await apiInstance.get<IncidentInterface[]>(`/exams/report`,
                 {
                     params : {
                         userId: userId,
--- model/examination/report.ts
+++ model/examination/report.ts
@@ -1,6 +1,12 @@
-export interface IncidantInterface {
+export enum IncidentType {
+    different_face = "different_face",
+    forbidden_object = "forbidden_object",
+}
+
+export interface IncidentInterface {
     type?: string,
     startingFrom: number
     endingAt: number
-    relatedMedia?: string
+    relatedMedia?: string,
+    incidentType: IncidentType
 }
