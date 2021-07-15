67c2bb28e4dc91e393764d9e2948f4e611d8d9d8
abdo hashem <ar1813@fayoum.edu.eg>
2021-07-15 18:17:09 +0200

    add exam duration

--- components/examination/create_exam/examform/start_end_time.tsx
+++ components/examination/create_exam/examform/start_end_time.tsx
@@ -6,11 +6,12 @@ import { Form } from "react-bootstrap";
 const date_iso8601 = "yyyy-MM-DDThh:mm"
 export default function Start_end_time() {
     const [startTime, setStartTime] = useState(moment().format(date_iso8601))
+    const [duration, setDuration] = useState(30) // default duration is 30 minutes
 
     return (
         <>
             <Form.Group controlId="validationCustom03">
-                <Form.Label>start time</Form.Label>
+                <Form.Label>availability start time</Form.Label>
                 <Form.Control name="startTime"
                     required
                     type="datetime-local"
@@ -20,21 +21,31 @@ export default function Start_end_time() {
                 />
             </Form.Group>
 
+            <Form.Group >
+                <Form.Label>duration in minutes</Form.Label>
+                <Form.Control name="duration"
+                    required
+                    type="number"
+                    min={1}
+                    value={duration}
+                    onChange={(event) => setDuration(parseInt(event.target.value))}
+                />
+            </Form.Group>
 
-            <Form.Group controlId="validationCustom03">
-                <Form.Label>end time</Form.Label>
+            <Form.Group >
+                <Form.Label>availability end time</Form.Label>
                 <Form.Control name="endTime"
                     required
                     type="datetime-local"
-                    min={date_minus_minute(startTime)}
-                    defaultValue={startTime}
+                    min={date_plus_minutes(startTime,duration)}
+                    defaultValue={date_plus_minutes(startTime,duration)}
                 />
             </Form.Group>
+
 
 
         </>
     )
 }
 const timeOffset = () => new Date().getTimezoneOffset() * 60000
-const now_iodate = (date=new Date()) => new Date(date.getTime() - timeOffset()).toISOString().slice(0, 16);
-const date_minus_minute = (time: string) => moment(time).add(5, 'minutes').format(date_iso8601);
+const date_plus_minutes = (time: string,minutes=1) => moment(time).add(minutes, 'minutes').format(date_iso8601);
